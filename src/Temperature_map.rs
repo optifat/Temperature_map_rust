5be0671faf3a8f7751367f40bf1186c8d8a29307
optifat <pavsmel@mail.ru>
2019-12-02 19:05:04 +0300

    temperature map in rust

---
+++ src/Temperature_map.rs
@@ -0,0 +1,46 @@
+#[derive(Copy, Clone)]
+pub struct Temperature_map {
+    dr: f64,
+    dt: f64,
+    Nx: i32,
+    Ny: i32,
+    Nz: i32,
+    absorb: f64,
+    D: f64,
+    k: f64,
+    Tm: i32,
+    p: f64,
+    a: f64,
+    v: f64,
+    T0: i32,
+}
+
+impl Temperature_map{
+    pub fn new(dr: f64, dt: f64, Nx: i32, Ny: i32, Nz: i32,
+    absorb: f64, D: f64, k: f64, Tm: i32, p: f64, a: f64, v: f64, T0: i32)->Temperature_map{
+
+        Temperature_map{
+            dr, dt, Nx, Ny, Nz, absorb, D, k, Tm, p, a, v, T0
+        }
+
+    }
+
+    pub fn integrate(self, x: f64, y: f64, z: f64) -> f64{
+        let mut integral: f64 = 0.0;
+        for t in 1..10000 {
+            let t = f64::from(t);
+
+            let power: f64 = ((x*self.dr+self.v*t*self.dt).powf(2.0) +
+            (y*self.dr).powf(2.0))/(4.0*self.D*t*self.dt+2.0*self.a*self.a) +
+            (z*self.dr).powf(2.0)/(4.0*self.D*t*self.dt);
+
+            let result = std::f64::consts::E.powf(-power);
+            //println!("{}", result);
+
+            integral += result * self.dt/(t*self.dt).powf(0.5)/(2.0*self.D*t*self.dt + self.a.powf(2.0)) *
+            self.absorb*self.p/(std::f64::consts::PI*self.k/self.D*(4.0*std::f64::consts::PI*self.D).powf(0.5));
+        }
+        //println!("{}", integral);
+        return integral
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,52 @@
+mod Temperature_map;
+
+fn main(){
+    let p: f64 = 100.0;
+    let dr: f64 = 5.0*10.0_f64.powf(-7.0_f64);
+    let dt: f64 = 60.0*10.0_f64.powf(-9.0_f64);
+    let Nx = 400;
+    let Ny = 100;
+    let Nz = 150;
+    let absorb = 0.25;
+    let D: f64 = 5.0*10.0_f64.powf(-6.0_f64);
+    let k: f64 = 24.0;
+    let Tm = 1563;
+    let size: f64 = 50.0*10.0_f64.powf(-6.0_f64);
+    let v = 0.8;
+    let T0 = 293;
+
+    let A = Temperature_map::Temperature_map::new(dr, dt, Nx, Ny, Nz, absorb, D, k, Tm, p, size, v, T0);
+
+    let mut temp_map: Vec<Vec<f64>> = Vec::with_capacity(Nx as usize);
+
+    for _ in 0..Nx{
+        let mut result: Vec<f64> = Vec::with_capacity(Nz as usize);
+        for _ in 0..Nz{
+            result.push(f64::from(T0))
+        }
+        temp_map.push(result);
+    }
+
+    for i in 0..Nx{
+        for j in 0..Nz{
+            temp_map[i as usize][j as usize] += A.integrate((i - Nx/2) as f64, 0.0, j as f64);
+        }
+    }
+
+    let mut depth: i32 = 0;
+
+    for i in 0..Nx{
+        for j in 0..Nz{
+            //println!("{}", temp_map[i as usize][j as usize]);
+            if temp_map[i as usize][j as usize] < Tm as f64{
+                if depth < j{
+                    depth = j;
+                }
+                break;
+            }
+        }
+    }
+
+    println!("{}", depth)
+
+}
